11e5ab25fe910a252ebab5725fa07cf226d4dcf6
machimachida <maatuudaa3@gmail.com>
2023-03-05 22:00:18 +0900

    Fix append benchmark

--- design-pattern/iterator/main_test.go
+++ design-pattern/iterator/main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 // 練習問題1-1のためのテスト
 
-// append()では内部で再アロケートすると思うが、明示的に書いたものとの速度の違いを確認
+// append()では内部で再アロケートすると思うが、自前で書いたものとの速度の違いを確認
 func BenchmarkAppendBook_Append(b *testing.B) {
 	bookShelf := NewBookShelf(4)
 	AppendBook := func(bs *BookShelf, b *Book) {
@@ -23,14 +23,63 @@ func BenchmarkAppendBook_Append(b *testing.B) {
 	}
 }
 
-// TODO: 要調査
-//
-// めちゃくちゃ遅い。メモリ開放されずに再定義しているから？
+// 毎度毎度Reallocateすると、めちゃくちゃ遅い。
 func BenchmarkAppendBook_Reallocate(b *testing.B) {
 	bookShelf := NewBookShelf(4)
 	AppendBook := func(bs *BookShelf, b *Book) {
 		if bs.last == len(bs.books) {
-			bs.books = make([]*Book, bs.last+1)
+			newbs := make([]*Book, bs.last+1)
+			copy(newbs, bs.books)
+			newbs[bs.last] = b
+			bs.books = newbs
+			bs.last++
+			return
+		}
+
+		bs.books[bs.last] = b
+		bs.last++
+	}
+
+	for i := 0; i < b.N; i++ {
+		AppendBook(bookShelf, &Book{"Example"})
+	}
+}
+
+// appendの実装と同じCapacityの伸ばし方をしている。appendより速い。
+func BenchmarkAppendBook_ReallocateDoubleCapacityAndLength(b *testing.B) {
+	bookShelf := NewBookShelf(4)
+	AppendBook := func(bs *BookShelf, b *Book) {
+		if bs.last == cap(bs.books) {
+			newbs := make([]*Book, bs.last*2)
+			copy(newbs, bs.books)
+			newbs[bs.last] = b
+			bs.books = newbs
+			bs.last++
+			return
+		}
+
+		bs.books[bs.last] = b
+		bs.last++
+	}
+
+	for i := 0; i < b.N; i++ {
+		AppendBook(bookShelf, &Book{"Example"})
+	}
+}
+
+// こちらはlenとcapの増やし方をappendと揃えたもの。
+// こちらもappendより速いが、よっぽどのことがない限りはappendを素直に使うべきだと思う。
+func BenchmarkAppendBook_ReallocateDoubleCapacity(b *testing.B) {
+	bookShelf := NewBookShelf(4)
+	AppendBook := func(bs *BookShelf, b *Book) {
+		if bs.last == cap(bs.books) {
+			newbs := make([]*Book, bs.last+1, bs.last*2)
+			copy(newbs, bs.books)
+			newbs[bs.last] = b
+			bs.books = newbs
+			bs.last++
+			return
+		} else if bs.last == len(bs.books) {
 			bs.books = append(bs.books, b)
 			bs.last++
 			return
